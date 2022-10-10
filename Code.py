7accb74f4ec9434ea1a0e7b5ba09de27345def00
Jmiran05 <114496706+Jmiran05@users.noreply.github.com>
2022-10-10 18:07:10 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,6 +1,6 @@
 def code(p1: str) -> int:
     #actually does the convertion but gives binary in alist
-    dictionary = {"A": 100000, "B": 100001, "C": 100010}  #char to bin
+    dictionary = {"A": 1000000, "B": 1000001, "C": 1000010}  #char to bin
     bin = []  #this is what will be returned//a list of the converted char into binary
     for char in p1:  #goes through every char in the string
         if char in dictionary: #checking to see if the char is actuallly given a val in the dictionary
