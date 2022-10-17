8e0ba2eaa0437f075d7abf549f712cde78223670
Jmiran05 <114496706+Jmiran05@users.noreply.github.com>
2022-10-17 00:30:29 -0400

    Update Team1Decode.py

--- Team1Decode.py
+++ Team1Decode.py
@@ -1,3 +1,4 @@
+open('Team1Code', 'w')
 def Decode(p1: str)->str:
     long_dictionary = {"1000000": "b", "1000001": "c", "1000010": "d", "1000100": "f", "1001000": "g", "1010000": "h",
                        "1100001": "k", "1100010": "l", "1100100": "m", "1101000": "p", "1110000": "q", "1110001": "v",
@@ -12,21 +13,18 @@ def Decode(p1: str)->str:
     short_dictionary = {"00000": "a", "00001": "e", "00010": "i", "00011": "o", "00100": "u", "00101": "r",
                         "00110": "s", "00111": "t", "01000": "n", "01001": "I", "01010": "T", "01011": " ",
                         "01100": ",", "01101": ".", "01110": "!", "01111": "?"}
-    p1 = p1.replace(p1[:3], "") #gets rid of 'D.' in 'D.B'
-    first_bin = p1[0]
+    p1 = p1.replace(p1[:3], "") #gets rid of the count and leaves only the bin which needs to be decoded
     result = ""
     while p1 != "":
+        first_bin = p1[0]
         if first_bin == "1": # if the first number is 1
             first_char = p1[:7] # look at the first 7 digit
             x = long_dictionary[first_char] # search for numbers in the long dictionary and turn them into characters
-            result = result + x  # add the letter to result
+            result = result + x  # output result in list called char
             p1 = p1[7:] #makes new p1 without the first bin
-        elif first_bin == "0": # if the first number is 0
+        elif first_bin == "0": # if the first number is 1
             first_char = p1[:5] # look at the first 5 digit
             x = short_dictionary[first_char] # search for numbers in the long dictionary and turn them into characters
-            result = result + x  #add the letter to result
+            result = result + x  # output result in list called char
             p1 = p1[5:] #makes new p1 without the first bin
     return result
-
-
-
